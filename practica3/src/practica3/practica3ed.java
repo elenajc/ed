af0e3c990ee38407a9d7f48a121b14fddade0e49
Elena Jimenez <elenajimenez03ebp@gmail.com>
2023-01-15 18:46:56 +0100

    Primer commit

--- practica3/src/practica3/practica3ed.java
+++ practica3/src/practica3/practica3ed.java
@@ -1,9 +1,21 @@
 package practica3;
+import java.util.Scanner;
 
 public class practica3ed {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 Scanner teclado = new Scanner (System.in);
+	        String cadena;
+	        boolean salir = false;
+	        while(!salir){
+	            System.out.println("Introduce una cadena");
+	            cadena=teclado.nextLine();
+	            System.out.println("Tamaño de la cadena es: "+cadena.length());
+	            if(cadena.length()==0){
+	                salir=true;
+	            }
+
+	        }
 
 	}
 
